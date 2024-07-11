bf74b26f81a7dc8eee13b5f8e8f79093213b2e0f
AKATWIJUKA-ELIA <eliaakjtrnq@gmail.com>
2024-07-11 21:49:34 +0300

    update2

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Typewriter } from 'nextjs-simple-typewriter'
 import Image from 'next/image'
+import { LuNetwork } from 'react-icons/lu'
+import { FaCode } from 'react-icons/fa'
 
 
 
@@ -15,21 +17,22 @@ const Hero = () => {
             <div className="fade-in-slowest text-center md:text-left md:ml-28 mt-10">
       
                         
-                  <h1 className="fade-in text-4xl md:text-6xl text-pink-400 font-bold">
+                  <h1 className="text-left text-3xl fade-in md:text-6xl text-pink-400 font-bold">
                   <span className='text-green-400'> THE </span> 
 
-                  LIGHT  <span className="text-blue-500 fade-in-slowest ">TECHNOLOGIES . . .</span>
+                  LIGHT  <span className="text-blue-500 md:text-6xl  fade-in-slowest ">TECHNOLOGIES . . .</span>
                   </h1>
+            
 
             </div>
 
-            <div className='text-left ml-28 mt-10'>
-                  <h1 className='text-4xl font-bold text-pink-400'>
-                  I'M
+            <div className='text-left ml-3 md:ml-28 mt-10'>
+                  <h1 className=' text-xl md:text-4xl font-bold text-pink-400'>
+                  I'M a
 
                   <span className='text-left text-blue-500'>
                   <Typewriter
-                        words={[' Software Engineer',' Web Developer and web designer',' Networker']}
+                        words={[' Software Engineer {%  %}',' Web Developer < / > and web designer',' Networker'] }
                         loop={100}
                         cursor
                         cursorStyle='_'
@@ -45,8 +48,8 @@ const Hero = () => {
 
             
 
-            <div className='text-left ml-28 mt-10 '>
-                  <h1 className='text-4xl'><span className='font-bold'> With the Increasingly developing technologies, We aim at providing Quality, durable and secure software solutions to our Customers </span> <br /> <br /> Feel free to explore our sevices...</h1>
+            <div className='text-left md:ml-28 mt-10 '>
+                  <h1 className='md:text-4xl'><span className='font-bold'> With the Increasingly developing technologies, We aim at providing Quality, durable and secure software solutions to our Customers </span> <br /> <br /> Feel free to explore our sevices...</h1>
             </div>
 
             
--- components/below-hero/index.tsx
+++ components/below-hero/index.tsx
@@ -7,26 +7,26 @@ const info = () => {
   return (
 
       <div className='  -mt-40  text-center md:text-left bg-blue-800 bg-opacity-25 '>
-            <div className='ml-24 flex gap-20 '>
+            <div className='md:ml-10 md:ml-24 flex flex-col md:flex md:flex-row gap-20 '>
             
             
-            <div className='flex flex-col mt-24 '>
-                 <h1 className='text-4xl font-semibold'>
-                  <span className='text-green-400 ml-32'>Hello ! </span> I'm Akatwijuka Elia
-                 </h1>
-
-                 <div className=' rounded-lg border border-black px-4 py-5  bg-opacity-25 bg-blue-900 backdrop-blur-md text-center md:text-left  '>
-                  <h1 className='text-2xl'>
-                  I'm a Proffessional Software engineer with a strong understanding <br /> of software development principles and passion for solving problems through code.
-                  <br /> I'm also a Full-stack web developer and Networker. <br /> With the experience i have attained in these fields 
-                  I can bring a holistic approach to your technological needs.
+                  <div className='flex flex-col md:mt-24 '>
+                  <h1 className='text-start md:text-center md:text-4xl font-semibold'>
+                        <span className='text-green-400 md:ml-32'>Hello ! </span> I'm Akatwijuka Elia
                   </h1>
-                 </div>
-            </div>
 
-            <div className='mr-24'>
-            <Image src={code} alt='code' width={500} height={200}/>
-            </div>
+                  <div className=' rounded-lg border border-black px-4 py-5  bg-opacity-25 bg-blue-900 backdrop-blur-md text-center md:text-left  '>
+                        <h1 className='text-2xl'>
+                        I'm a Proffessional Software engineer with a strong understanding <br /> of software development principles and passion for solving problems through code.
+                        <br /> I'm also a Full-stack web developer and Networker. <br /> With the experience i have attained in these fields 
+                        I can bring a holistic approach to your technological needs.
+                        </h1>
+                  </div>
+                  </div>
+
+                  <div className='mr-24'>
+                  <Image src={code} alt='code' width={500} height={200}/>
+                  </div>
 
             </div>
             
