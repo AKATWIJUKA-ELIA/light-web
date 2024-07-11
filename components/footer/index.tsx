eaef1cadb748a6fece18b885fec26f2e12d35c4e
AKATWIJUKA-ELIA <eliaakjtrnq@gmail.com>
2024-07-11 22:13:16 +0300

    added links

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -2,43 +2,47 @@ import React from 'react'
 import Link from 'next/link'
 import { BsGithub } from 'react-icons/bs'
 import { BsLinkedin } from 'react-icons/bs'
+import { BsInstagram } from 'react-icons/bs'
+import { BsTwitter } from 'react-icons/bs'
+import { BsSlack } from 'react-icons/bs'
+import { BsFacebook } from 'react-icons/bs'
 
 const footer = () => {
   return (
-      <div className=" mx-40 py-2 grid rounded-full backdrop-blur-sm text-center grid-cols-5 md:text-left  bg-gradient-to-r from-indigo-600 via-purple-700 to-pink-400">
+      <div className=" md:mx-40 md:py-2 flex mx-5  rounded-full backdrop-blur-sm text-center md:grid  md:grid-cols-5 md:text-left  bg-gradient-to-r from-indigo-600 via-purple-700 to-pink-400">
       <a
-        href=""
-        className="group rounded-lg border border-transparent mx-10 px-5 py-4 "  >
+        href="https://github.com/AKATWIJUKA-ELIA"
+        className="group rounded-lg border border-transparent md:mx-10 px-5 py-4 "  >
             <BsGithub/>
       
       </a>
 
       <a
         href=""
-        className="group rounded-lg border border-transparent mx-10 px-5 py-4 "  >
-            <BsGithub/>
-      
-      </a>
-
-      <a
-        href=""
-        className="group rounded-lg border border-transparent mx-10 px-5 py-4 "  >
-            <BsGithub/>
-      
-      </a>
-
-
-      <a
-        href=""
-        className="group rounded-lg border border-transparent mx-10 px-5 py-4 "  >
-            <BsGithub/>
-      
-      </a>
-
-      <a
-        href=""
-        className="group rounded-lg border border-transparent mx-10 px-5 py-4 "  >
+        className="group rounded-lg border border-transparent md:mx-10 px-5 py-4 "  >
             <BsLinkedin/>
+      
+      </a>
+
+      <a
+        href="https://x.com/The_light_tech"
+        className="group rounded-lg border border-transparent md:mx-10 px-5 py-4 "  >
+            <BsTwitter/>
+      
+      </a>
+
+
+      <a
+        href="https://stackoverflow.com/users/22681878"
+        className="group rounded-lg border border-transparent md:mx-10 px-5 py-4 "  >
+            <BsSlack/>
+      
+      </a>
+
+      <a
+        href=""
+        className="group rounded-lg border border-transparent md:mx-10 px-5 py-4 "  >
+            <BsFacebook/>
       
       </a>
       
