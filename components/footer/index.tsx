82756aba56853699d695e351066cb30307bb92d6
AKATWIJUKA-ELIA <eliaakjtrnq@gmail.com>
2024-10-01 00:09:25 +0300

    update

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,84 +1,93 @@
+"use client"
 import React from 'react'
-import Link from 'next/link'
-import { BsGithub } from 'react-icons/bs'
+import { useState } from 'react'
 import { HiUser } from 'react-icons/hi'
 import { FaMessage } from 'react-icons/fa6' 
 import { HiMiniServerStack } from 'react-icons/hi2'
 import { AiOutlineFileDone } from 'react-icons/ai'
-import { BsWhatsapp } from 'react-icons/bs'
 import { HiHome } from 'react-icons/hi'
 
+
 const footer = () => {
+
+ const Click = () =>{
+      document.getElementById('services')?.classList.remove('icon-color');
+      document.getElementById('projects')?.classList.remove('icon-color');
+      document.getElementById('about')?.classList.remove('icon-color');
+      document.getElementById('contact')?.classList.remove('icon-color');
+      document.getElementById('home')?.classList.add('icon-color');
+ }
+
   return (
       <div className="fixed bottom-0 left-0 z-40 flex justify-center gap-6 md:hidden w-full border opacity-75 rounded-full backdrop-blur-lg ">
 
-      <div className='flex flex-col'>
-      <a
-        href="/"
-        className="group rounded-lg border border-transparent md:mx-10 px-5 py-4 md:hidden"  >
-            <HiHome className='text-2xl'/>
-      
-      </a>
-
-      <h1 className='flex mx-auto -mt-5'>
-            Home
-      </h1>
-      </div>
-
-
-      <div className='flex flex-col'>
-      <a    href="/#services"
-        className="group rounded-lg border border-transparent md:mx-10 px-5 py-4 md:hidden"  >
-            <HiMiniServerStack className='text-2xl text-red-800 flex'/>
-            
-      
-      </a>
-
-      <h1 className='flex mx-auto -mt-5'>
-            Services
-      </h1>
-      </div>
-     
-     
-
-      
-      <div className='flex flex-col'>
-      <a
-        href="/projects"
-        className="group rounded-lg border border-transparent md:mx-10 px-5 py-4 md:hidden"  >
-            <AiOutlineFileDone className='text-2xl text-red-800'/>
-      
-      </a>
-
-      <h1 className='flex mx-auto -mt-5'>
-            Projects
-      </h1>
-      </div>
-
-
-      <div className='flex flex-col '>
-      <a
-        href="/about"
-        className="group rounded-lg border border-transparent md:mx-10 px-5 py-4 md:hidden"  >
-            <HiUser className='text-2xl text-red-800'/>
-      
-      </a>
-
-      <h1 className='flex mx-auto -mt-5'>
-            About
-      </h1>
-      </div>
-
-
-      <div className='flex flex-col '>
+      <div className='flex flex-col' id='home' >
             <a
-            href="/contact"
+            href="/"
             className="group rounded-lg border border-transparent md:mx-10 px-5 py-4 md:hidden"  >
-                  <FaMessage className='text-2xl text-red-800'/>
+                  <HiHome className='text-2xl ' id='home' onClick={Click}/>
             
             </a>
 
-            <h1 className='flex mx-auto -mt-5'>
+            <h1 className='flex mx-auto -mt-5 '>
+                  Home
+            </h1>
+      </div>
+
+
+      <div className='flex flex-col md:hidden' id='services'>
+            <a    href="/#services"
+            className="group rounded-lg border border-transparent md:mx-10 px-5 py-4 md:hidden"  >
+                  <HiMiniServerStack className='text-2xl icon-color flex'/>
+                  
+            
+            </a>
+
+            <h1 className='flex  mx-auto -mt-5 '>
+                  Services
+            </h1>
+      </div>
+     
+     
+
+      
+      <div className='flex flex-col' id='projects'>
+            <a
+            href="/projects"
+            className="group rounded-lg border border-transparent md:mx-10 px-5 py-4 md:hidden"  >
+                  <AiOutlineFileDone className='text-2xl icon-color'/>
+            
+            </a>
+
+            <h1 className='flex mx-auto -mt-5 '>
+                  Projects
+            </h1>
+      </div>
+
+
+      <div className='flex flex-col' id='about'>
+            <a
+            href="/about"
+            className="group rounded-lg border border-transparent md:mx-10 px-5 py-4 md:hidden"  >
+                  <HiUser className='text-2xl icon-color'/>
+            
+            </a>
+
+            <h1 className='flex mx-auto -mt-5 '>
+                  About
+            </h1>
+      </div>
+
+
+      <div className='flex flex-col ' id='contact'>
+            <a
+            href="/contact"
+            className="group rounded-lg border border-transparent md:mx-10 px-5 py-4 md:hidden"  >
+                  <FaMessage className='text-2xl icon-color'/>
+            
+            </a>
+
+            <h1 className='flex mx-auto -mt-5 '>
                   Contact
             </h1>
       </div>
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -13,16 +13,18 @@ const Projects = () => {
   return (
     <div className='top-0 justify-between items-center min-h-screen ' style={{ backgroundImage:`url('/images/banner-bg.png')`,backgroundRepeat:'no-repeat',backgroundSize:'fill',backgroundPosition:'center', backgroundBlendMode:'multiply'}} >
 
-      <div className='flex   justify-center gap-20 items-center '>
-            <div className='flex flex-col ml-10 mt-10'>
-            <h1 className='text-5xl font-bold mb-10 slider slide--fast'> What I Have <span className='text-pink-400'>Done</span></h1>
+      <div className='flex flex-col md:flex-row   justify-center md:gap-20 items-center '>
 
-            <h1 className='text-2xl font-bold slider slide--slow'> Explore and rate the various projects ive worked on </h1>
 
-            <h1  className='mt-10 slider slide--slower'>Contact Us to get a tailored service specifically to meet your particular needs </h1>
+            <div className='flex flex-col  md:ml-10 mt-40 md:mt-10'>
+                  <h1 className='text-3xl md:text-5xl font-bold md:mb-10 slider slide--fast text-center'> What I Have <span className='text-pink-400'>Done</span></h1>
+
+                  <h1 className='text-2xl font-bold slider slide--slow mx-10 mt-3 md:mt-0'> Explore and rate the various projects ive worked on </h1>
+
+                  <h1  className='mt-10 slider slide--slower pl-3'>Contact Us to get a tailored service specifically to meet your particular needs </h1>
             </div>
 
-            <div className=' md:mb-10 flex px-2 py-3 grid grid-cols-2 gap-3 mt-52 backdrop-blur-sm drop-shadow-lg rounded-lg slider-right slide--fast ' >
+            <div className=' md:mb-10 flex px-2 py-3 grid grid-cols-2 gap-3 md:mt-52 backdrop-blur-sm drop-shadow-lg rounded-lg slider-right slide--fast ' >
                   
                   <div className="flex bg-blue-200 border rounded-lg">
                         <Link href='https://eliatranquil.pythonanywhere.com/'>
