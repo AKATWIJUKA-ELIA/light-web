9bea95b1ef6501bc3f4726e62d592e12bcc150f4
AKATWIJUKA-ELIA <eliaakjtrnq@gmail.com>
2024-10-25 21:20:08 +0300

    update

--- components/testimonial/index.tsx
+++ components/testimonial/index.tsx
@@ -20,26 +20,25 @@ const testimonial = () => {
 
             <figcaption className="mt-10">
                   <img
-                  alt="ee"
-                  src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                  className="mx-auto h-20 w-20 md:h-40 md:w-40 rounded-full"
+                  alt="troy"
+                  src="images/troy.png"
+                  className="mx-auto py-5 h-20 w-20 md:h-40 md:w-40 rounded-full"
                   />
 
                   <div className="mt-4 flex flex-col items-center justify-center space-x-3 text-base">
-                  <div className="font-semibold text-gray-900">Judith Black</div>
+                  <div className="font-semibold text-gray-900"> TroyLegacy</div>
                   {/* <svg width={3} height={3} viewBox="0 0 2 2" aria-hidden="true" className="fill-gray-900">
                   <circle r={5} cx={5} cy={5} />
                   </svg> */}
-                  <div className="text-gray-600">CEO of Workcation</div>
+                  <div className="text-gray-600">Partner</div>
                   </div>
             </figcaption>
             
             
             
-            <blockquote className=" md:mt-20 text-center md:text-xl font-semibold leading-8 text-gray-900 sm:text-2xl sm:leading-9">
+            <blockquote className="md:ml-20 md:mt-20 text-center md:text-xl font-semibold leading-8 text-gray-900 sm:text-2xl sm:leading-9">
                   <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo expedita voluptas culpa sapiente alias
-                  molestiae. Numquam corrupti in laborum sed rerum et corporis.
+                  The services that light tech provides are top notch. They are dedicated to extending creativity and innovation into digital solutions that benefit the global community.
                   </p>
             </blockquote>
 
