137bcb42829f40dbf94d2035cee3e7c3b484b2a3
AKATWIJUKA-ELIA <eliaakjtrnq@gmail.com>
2024-10-25 12:11:04 +0300

    education

---
+++ components/about/education.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const Education = () => {
+  return (
+    <div className='mr-5 gap-4 flex flex-col border rounded-lg backdrop-blur-3xl'>
+      <div className='flex gap-6 -ml-10 mr-12'>
+            <div className='font-bold rounded-md text-xl  flex h-8 w-16'> <h1>Degree:</h1></div>
+            <div className='flex'> <h1 className='text-white pr-4 ' > I earned a Bachelor of Science in Software Engineering from Bugema University in Year. <br/> My studies focused on software development methodologies, programming languages, and database management. <br/>I am proficient in list of programming languages and have experience in list of relevant projects.    </h1></div>
+      
+      </div>
+
+      <div className='flex gap-6 -ml-10 mr-12'>
+            <div className='font-bold rounded-md text-xl  flex h-8 w-16'> <h1>Degree:</h1></div>
+            <div className='flex'> <h1 className='text-white pr-4 ' > I earned a Bachelor of Science in Software Engineering from Bugema University in Year. <br/> My studies focused on software development methodologies, programming languages, and database management. <br/>I am proficient in list of programming languages and have experience in list of relevant projects.    </h1></div>
+      
+      </div>
+
+      <div className='flex gap-6 -ml-10 mr-12'>
+            <div className='font-bold rounded-md text-xl bg-gradient-to-r from-blue-800 to-pink-800 bg-opacity-25 flex h-8 w-36'> <h1>High School:</h1></div>
+            <div className='flex'> <h1 className='text-white pr-4 ' > I hold a Bachelors Degree in Software Engineering which i atained at Bugema University    </h1></div>
+      
+      </div>
+
+     
+     
+      
+    </div>
+  )
+}
+
+export default Education;
--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React from 'react'
+import Education from '../../../components/about/education';
 
 const about = () => {
   return (
@@ -11,7 +12,7 @@ const about = () => {
                   <div className='flex flex-col '>
                         <div className='flex justify-center '>
                         <h1 className='text-3xl md:text-7xl font-bold'>
-                        <span className='md:hidden text-red-500'><a href="/">Home/</a></span>About Us
+                        <span className='md:hidden text-red-500'><a href="/">Home/</a></span>About Me
                         </h1>
                         </div>
 
@@ -21,32 +22,37 @@ const about = () => {
                         
                   </div>
 
-                  <div className='flex mx-5  -mt-10 md:-mt-80 '>
-                        <div className='flex gap-10'>
-                              <div className='flex'>
+                  <div className='flex flex-col mx-5  -mt-10  '>
+                        <div className='flex gap-10 px-1'>
+                              <div className='flex activate rounded '>
                                     <h1 className='pointer'>
                                           Education
                                     </h1>
                               </div>
 
-                              <div className='flex'>
+                              <div className='flex rounded'>
                                     <h1 className='pointer'>Skills</h1>
                               </div>
 
-                              <div className='flex'>
+                              <div className='flex rounded'>
                                     <h1 className='pointer'>Achievements</h1>
                               </div>
 
-                              <div className='flex'>
+                              <div className='flex rounded'>
                                     <h1 className='pointer'>
                                     Interests
+                                    </h1>
+                              </div>
+                              <div className='flex rounded'>
+                                    <h1 className='pointer'>
+                                    Work Experience
                                     </h1>
                               </div>
 
                         </div>
 
-                        <div>
-
+                        <div className='flex -ml-4 mt-3'>
+                        <Education />
                         </div>
 
                   </div>
